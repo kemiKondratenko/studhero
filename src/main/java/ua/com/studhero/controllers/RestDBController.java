ca96fbb3d08c9c5f65795c1e24d238f6484fc900
Eugene <kemi.kondratenko@gmail.com>
2015-09-09 01:31:01 +0300

    added injection dependency remove bullshit

--- src/main/java/ua/com/studhero/controllers/RestDBController.java
+++ src/main/java/ua/com/studhero/controllers/RestDBController.java
@@ -1,20 +1,13 @@
 package ua.com.studhero.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import ua.com.studhero.annotations.ClassId;
 import ua.com.studhero.database.DataBaseWorker;
 import ua.com.studhero.database.entities.Example;
-import ua.com.studhero.database.impl.ConnectorImpl;
-import ua.com.studhero.database.impl.DataBaseWorkerImpl;
 
 import javax.inject.Inject;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 /**
@@ -31,15 +24,13 @@ public class RestDBController {
 
     @RequestMapping(value="/getFromDB", method = RequestMethod.GET)
     public @ResponseBody
-    String getFromDB(){
-        /*ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        DataBaseWorker merchantService = context.getBean(DataBaseWorkerImpl.class);*/
-        log.info("START");
+    Example getFromDB(){
         try {
-            return " "+dataBaseWorker.get(2l, Example.class).field;
+            return dataBaseWorker.get(2l, Example.class);
         } catch (Exception e) {
-            return " ";
+            e.printStackTrace();
         }
+        return null;
     }
 
     public void setDataBaseWorker(DataBaseWorker dataBaseWorker) {
