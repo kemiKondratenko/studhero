825592e9c4f88da4c5e018ec0b111d419ad1770b
Kasp Yar <kaspyar@yahoo.com>
2015-09-10 00:21:27 +0300

    added rest mock

--- src/main/java/ua/com/studhero/controllers/RestDBController.java
+++ src/main/java/ua/com/studhero/controllers/RestDBController.java
@@ -1,13 +1,15 @@
 package ua.com.studhero.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ua.com.studhero.database.DataBaseWorker;
 import ua.com.studhero.database.entities.Example;
+import ua.com.studhero.database.impl.DataBaseWorkerMock;
+import ua.com.studhero.model.entity.Company;
+import ua.com.studhero.model.entity.Event;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -21,17 +23,32 @@ public class RestDBController {
 
     @Inject
     private DataBaseWorker dataBaseWorker;
+    private DataBaseWorkerMock dataBaseWorkerMock = new DataBaseWorkerMock();
 
-    @RequestMapping(value="/getFromDB", method = RequestMethod.GET)
+    @RequestMapping(value="/events/getEvents", method = RequestMethod.GET)
     public @ResponseBody
-    Example getFromDB(){
-        try {
-            return dataBaseWorker.get(2l, Example.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    List<Event> getFromDB(){
+        return dataBaseWorkerMock.getAllEvents();
     }
+
+    @RequestMapping(value="/companies", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Company> getCompaniesFromDB(){
+        return dataBaseWorkerMock.getAllCompanies();
+    }
+
+    @RequestMapping(value = "/events/createEvent", method = RequestMethod.POST)
+    public @ResponseBody Event createEvent(@RequestBody Event event) {
+        dataBaseWorkerMock.addEvent(event);
+        return event;
+    }
+
+
+    @RequestMapping(value = "/events/deleteEvent", method = RequestMethod.POST)
+    public @ResponseBody String createEmployee(@RequestBody Long eventId) {
+        return dataBaseWorkerMock.removeEvent(eventId);
+    }
+
 
     public void setDataBaseWorker(DataBaseWorker dataBaseWorker) {
         this.dataBaseWorker = dataBaseWorker;
