3e2265d1c15e9e6760f3625680eaf4793cde552f
Kasp Yar <kaspyar@yahoo.com>
2015-09-17 22:12:21 +0300

    added creating tag and getting all tags

--- src/main/java/ua/com/studhero/model/entity/Tag.java
+++ src/main/java/ua/com/studhero/model/entity/Tag.java
@@ -1,11 +1,15 @@
 package ua.com.studhero.model.entity;
 
+import ua.com.studhero.annotations.AttrId;
+import ua.com.studhero.annotations.ClassId;
 import ua.com.studhero.database.entities.BaseDBO;
 
 /**
  * @author - KaspYar
  */
+@ClassId(id = 10)
 public class Tag extends BaseDBO{
+    @AttrId(id = 45)
     private String tag;
 
     public Tag() {
