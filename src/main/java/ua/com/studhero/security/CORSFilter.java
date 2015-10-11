e420df74279b25542c7addc76ea3919957f66e9f
Kasp Yar <kaspyar@yahoo.com>
2015-10-11 19:53:44 +0300

    [undoc] - StudHero - add studhero.org to CORS

--- src/main/java/ua/com/studhero/security/CORSFilter.java
+++ src/main/java/ua/com/studhero/security/CORSFilter.java
@@ -16,6 +16,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "http://89.184.67.220");
+        response.setHeader("Access-Control-Allow-Origin", "http://studhero.org");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
